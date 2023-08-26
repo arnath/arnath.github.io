2540a116b39ab3214444eef5f8d9845f204a5be0
Vijay Prakash <prakash.vijay@gmail.com>
2023-08-26 22:05:54 +0200

    Set up a landing page for full screen size

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,48 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+</script>
+
+<div class="container center-self">
+  <div class="links">
+    <div class="title">vijayp.dev</div>
+    <a class="gap" href="/about">About</a>
+    <a href="/blog">Blog</a>
+    <a href="/projects">Projects</a>
+  </div>
+  <div class="tagline">
+    Backend engineer with over 10 years of experience working on high scale distributed systems
+  </div>
+</div>
+
+<style>
+  .container {
+    display: flex;
+    flex-direction: row;
+    gap: 2rem;
+
+    margin-top: 10vh;
+    max-width: 40rem;
+  }
+
+  .links {
+    display: flex;
+    flex-direction: column;
+    gap: 0.75em;
+  }
+
+  .title {
+    color: #0066b2;
+    font-size: 48px;
+    line-height: 48px;
+    font-weight: bold;
+  }
+
+  a {
+    text-decoration: none;
+    color: #0066b2;
+    font-size: 20px;
+  }
+
+  .gap {
+    margin-top: 1em;
+  }
+</style>
--- src/routes/blog/+layout.svelte
+++ src/routes/blog/+layout.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
-  import "../app.css";
+  import NavBar from "$lib/components/NavBar.svelte";
 </script>
 
+<NavBar />
 <main>
   <slot />
 </main>
