cd875b6c3f49cecd659c18bf1c9ca8309f5ab89a
Vijay Prakash <prakash.vijay@gmail.com>
2023-08-26 19:39:16 +0200

    Set up basic root layout

---
+++ src/lib/components/NavBar.svelte
@@ -0,0 +1,25 @@
+<header>
+  <a href="#top"><h2>vijayp.dev</h2></a>
+  <a href="https://www.paypal.com/donate/?business=4E9UH7HNM2GP8&no_recurring=0&currency_code=USD">Donate</a>
+</header>
+
+<style>
+  header {
+    background-color: #107c10;
+    position: fixed;
+    top: 0;
+    width: 100%;
+    z-index: 1;
+
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 1.5rem 0 1rem;
+  }
+
+  a {
+    text-decoration: none;
+    color: #ffffff;
+  }
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import "../app.css";
+  import NavBar from "$lib/components/NavBar.svelte";
+</script>
+
+<NavBar />
+<main>
+  <slot />
+</main>
+
+<style>
+  main {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+
+    margin: 60px auto auto auto;
+    max-width: 60rem;
+    padding-top: 1em;
+    padding-bottom: 1em;
+  }
+</style>
