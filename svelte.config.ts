a47a66de471ca960d457cb1a72d08785ecdd446d
Vijay Prakash <prakash.vijay@gmail.com>
2023-08-26 14:56:01 +0200

    Update Prettier config and format all files

--- svelte.config.ts
+++ svelte.config.ts
@@ -1,21 +1,17 @@
-import adapter from '@sveltejs/adapter-auto';
-import { Config } from '@sveltejs/kit';
-import { vitePreprocess } from '@sveltejs/kit/vite';
-import { mdsvex } from 'mdsvex'
+import adapter from "@sveltejs/adapter-auto";
+import { Config } from "@sveltejs/kit";
+import { vitePreprocess } from "@sveltejs/kit/vite";
+import { mdsvex } from "mdsvex";
 
 export default {
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
-  preprocess: [
-    vitePreprocess(),
-    mdsvex({ extensions: ['.md'] }),
-  ],
+  preprocess: [vitePreprocess(), mdsvex({ extensions: [".md"] })],
   kit: {
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter()
+    adapter: adapter(),
   },
-  extensions: ['.svelte', '.md'],
-
+  extensions: [".svelte", ".md"],
 } satisfies Config;
