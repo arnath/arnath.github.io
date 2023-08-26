dd42ff8148c3222db8466e9b84e49e1123a25010
Vijay Prakash <prakash.vijay@gmail.com>
2023-08-26 20:42:58 +0200

    Got a basic layout I like working

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -1,21 +1,31 @@
 <header>
-  <a href="#top"><h2>vijayp.dev</h2></a>
-  <a href="https://www.paypal.com/donate/?business=4E9UH7HNM2GP8&no_recurring=0&currency_code=USD">Donate</a>
+  <div class="container">
+    <a href="#top"><h2>vijayp.dev</h2></a>
+    <a href="https://www.paypal.com/donate/?business=4E9UH7HNM2GP8&no_recurring=0&currency_code=USD"
+      >Donate</a>
+  </div>
 </header>
 
 <style>
   header {
-    background-color: #107c10;
+    background-color: #0066b2;
     position: fixed;
     top: 0;
     width: 100%;
     z-index: 1;
 
     display: flex;
+    justify-content: center;
+  }
+
+  .container {
+    display: flex;
     flex-direction: row;
     justify-content: space-between;
     align-items: center;
-    padding: 0 1.5rem 0 1rem;
+    
+    max-width: 60rem;
+    width: 100%;
   }
 
   a {
