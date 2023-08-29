3bbae11a34be1089224251a9d651b10f9d41e903
Vijay Prakash <prakash.vijay@gmail.com>
2023-08-29 22:43:47 +0200

    Add .nojekyll file

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/kit/vite";
 import { mdsvex } from "mdsvex";
 
@@ -14,9 +14,9 @@ export default {
   extensions: [".svelte", ".md"],
   preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
   kit: {
-    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
-    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
+    paths: {
+        base: process.env.NODE_ENV === 'production' ? '/sveltekit-github-pages' : '',
+    }
   },
 };
