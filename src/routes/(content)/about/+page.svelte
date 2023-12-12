5e92e3e6d51a1217c82e1c95a9fedcf10c8a2853
Vijay Prakash <prakash.vijay@gmail.com>
2023-12-13 00:01:25 +0100

    Fix tenure

--- src/routes/(content)/about/+page.svelte
+++ src/routes/(content)/about/+page.svelte
@@ -5,6 +5,7 @@
 <div class="container">
   <h1>About</h1>
   <p>
+    <!-- Don't forget to update the other number in the Home page! -->
     Hi, I'm Vijay! I have been coding my whole life and working as a professional
     software engineer for over 12 years at some of the best companies in the world.
     I'm primarily a backend engineer although I'm starting to dabble in frontend
