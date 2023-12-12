d13fb8257922736ab46aa9f450ba60417171c049
Vijay Prakash <prakash.vijay@gmail.com>
2023-12-12 23:04:37 +0100

    Add social buttons

--- src/routes/(content)/about/+page.svelte
+++ src/routes/(content)/about/+page.svelte
@@ -1,11 +1,19 @@
+<script lang="ts">
+  import AboutPageSocialLink from '$lib/components/AboutPageSocialLink.svelte';
+</script>
+
 <div class="container">
   <h1>About</h1>
   <p>
     Hi, I'm Vijay! I have been coding my whole life and working as a professional
-    software engineer for over 10 years at some of the best companies in the world.
+    software engineer for over 12 years at some of the best companies in the world.
     I'm primarily a backend engineer although I'm starting to dabble in frontend
     (hence this website). Links to my various profiles can be found below. 
   </p>
+  <ul>
+    <AboutPageSocialLink network="github" url="https://www.github.com/arnath" />
+    <AboutPageSocialLink network="linkedin" url="https://www.linkedin.com/in/vpdev/" />
+  </ul>
 </div>
 
 <style>
@@ -13,5 +21,9 @@
     display: flex;
     flex-direction: column;
     gap: 1em;
+  }
+
+  ul {
+    list-style: none;
   }
 </style>
