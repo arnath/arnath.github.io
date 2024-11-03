c547a4bb037997364951108b8841cfed97c436ff
Vijay Prakash <prakash.vijay@gmail.com>
2024-11-03 21:57:39 +0100

    Add some stuff

--- src/lib/supabase-client.ts
+++ src/lib/supabase-client.ts
@@ -1,27 +1,38 @@
-import { createClient, type SupabaseClient as SupabaseInternalClient } from '@supabase/supabase-js';
-import type { Job } from './types';
+import {
+    createClient,
+    type SupabaseClient as SupabaseInternalClient,
+} from "@supabase/supabase-js";
+import type { Job } from "./types";
 
 export class SupabaseClient {
     supabaseClient: SupabaseInternalClient;
 
     constructor() {
-        this.supabaseClient = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_KEY);
+        this.supabaseClient = createClient(
+            import.meta.env.VITE_SUPABASE_URL,
+            import.meta.env.VITE_SUPABASE_KEY
+        );
     }
 
     async fetchJobs(queryString: string | null): Promise<Job[]> {
         let searchPromise;
         if (queryString) {
-            searchPromise = this.supabaseClient.from('jobs').select().textSearch('fts', queryString);
+            searchPromise = this.supabaseClient
+                .from("jobs")
+                .select()
+                .textSearch("fts", queryString);
         } else {
-            searchPromise = this.supabaseClient.from('jobs').select();
+            searchPromise = this.supabaseClient.from("jobs").select();
         }
 
         const { data, error } = await searchPromise;
         if (error) {
-            console.log(`Error response from Supabase: ${error.code} ${error.message} ${error.hint} ${error.details}`);
+            console.log(
+                `Error response from Supabase: ${error.code} ${error.message} ${error.hint} ${error.details}`
+            );
         }
 
-        const jobs = data as Job[] ?? [];
+        const jobs = (data as Job[]) ?? [];
 
         return jobs;
     }
