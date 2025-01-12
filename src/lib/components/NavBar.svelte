485decd24188492528b6299008de57c380c28dea
Vijay Prakash <prakash.vijay@gmail.com>
2025-01-11 22:19:20 -0500

    Add new social icons to the nav bar

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -1,11 +1,19 @@
 <script lang="ts">
   import { base } from "$app/paths";
+  import SocialIcons from "@rodneylab/svelte-social-icons";
 </script>
 
 <header>
-  <a class="title" href="{base}/">vijayp.dev</a>
-  <a href="{base}/blog">Blog</a>
-  <a href="{base}/projects">Projects</a>
+  <div class="left">
+    <a class="title" href="{base}/">vijayp.dev</a>
+    <a href="{base}/blog">Blog</a>
+    <a href="{base}/projects">Projects</a>
+  </div>
+  <div class="right">
+    <a href="https://github.com/arnath"><SocialIcons network="github" width="28" height="28" /></a>
+    <a href="https://www.linkedin.com/in/vpdev"
+      ><SocialIcons network="linkedin" width="28" height="28" /></a>
+  </div>
 </header>
 
 <style>
@@ -20,8 +28,23 @@
 
     display: flex;
     flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .left {
+    display: flex;
+    flex-direction: row;
     justify-content: start;
     align-items: baseline;
+    gap: 1em;
+  }
+
+  .right {
+    display: flex;
+    flex-direction: row;
+    justify-content: end;
+    align-items: center;
     gap: 1em;
   }
 
