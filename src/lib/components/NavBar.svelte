653a53797d96418398effb7d9c744194db8731c6
Vijay Prakash <prakash.vijay@gmail.com>
2025-01-12 16:56:23 -0500

    Rebuilt the project on SvelteKit 2 (#29)
    I rebuilt the project on SvelteKit 2 because I hadn't touched it in a
    while and that seemed easier than figuring out how to migrate. The
    changes were relatively minor. Also added the Flan project and made some
    changes to the structure of the site.

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -1,12 +1,19 @@
 <script lang="ts">
   import { base } from "$app/paths";
+  import SocialIcons from "@rodneylab/svelte-social-icons";
 </script>
 
 <header>
+  <div class="left">
     <a class="title" href="{base}/">vijayp.dev</a>
-    <a href="{base}/about">About</a>
     <a href="{base}/blog">Blog</a>
     <a href="{base}/projects">Projects</a>
+  </div>
+  <div class="right">
+    <a href="https://github.com/arnath"><SocialIcons network="github" width="28" height="28" /></a>
+    <a href="https://www.linkedin.com/in/vpdev"
+      ><SocialIcons network="linkedin" width="28" height="28" /></a>
+  </div>
 </header>
 
 <style>
@@ -16,13 +23,28 @@
     width: 100%;
     z-index: 1;
     padding: 1rem 0;
-    background-color: #EDF2F8;
+    background-color: #edf2f8;
     font-size: 20px;
 
     display: flex;
     flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .left {
+    display: flex;
+    flex-direction: row;
     justify-content: start;
     align-items: baseline;
+    gap: 1em;
+  }
+
+  .right {
+    display: flex;
+    flex-direction: row;
+    justify-content: end;
+    align-items: center;
     gap: 1em;
   }
 
---
+++ svelte.config.ts
@@ -0,0 +1,22 @@
+import { mdsvex } from "mdsvex";
+import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import type { Config } from "@sveltejs/kit";
+import type { MdsvexOptions } from "mdsvex";
+
+const mdsvexOptions: MdsvexOptions = {
+  extensions: [".md"],
+};
+
+const config: Config = {
+  // Consult https://svelte.dev/docs/kit/integrations
+  // for more information about preprocessors
+  preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
+  kit: {
+    adapter: adapter(),
+  },
+
+  extensions: [".svelte", ".svx", ".md"],
+};
+
+export default config;
