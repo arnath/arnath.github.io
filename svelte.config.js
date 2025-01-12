57dea371b3a097295c0312d13a338b5d7bb4fa96
Vijay Prakash <prakash.vijay@gmail.com>
2025-01-11 21:04:22 -0500

    Initial commit of Sveltekit 2 project

--- svelte.config.js
+++ svelte.config.js
@@ -1,24 +1,21 @@
-import adapter from "@sveltejs/adapter-static";
-import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
-import { mdsvex } from "mdsvex";
-
-/** @type {import('mdsvex').MdsvexOptions} */
-const mdsvexOptions = {
-  extensions: [".md"],
-};
-
-const dev = process.argv.includes("dev");
+import { mdsvex } from 'mdsvex';
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
-export default {
-  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+const config = {
+  // Consult https://svelte.dev/docs/kit/integrations
   // for more information about preprocessors
-  extensions: [".svelte", ".md"],
-  preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
+  preprocess: [vitePreprocess(), mdsvex()],
+
   kit: {
-    adapter: adapter(),
-    paths: {
-      base: dev ? "" : process.env.BASE_PATH,
-    },
+    // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+    // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+    // See https://svelte.dev/docs/kit/adapters for more information about adapters.
+    adapter: adapter()
   },
+
+  extensions: ['.svelte', '.svx']
 };
+
+export default config;
